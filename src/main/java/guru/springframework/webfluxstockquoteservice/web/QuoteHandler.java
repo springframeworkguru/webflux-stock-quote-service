37f67eeb1fc57232f18c3a0f44a4b681c71f0954
John Thompson <john@springframework.guru>
2017-10-18 12:51:32 -0400

    adding quote handler

---
+++ src/main/java/guru/springframework/webfluxstockquoteservice/web/QuoteHandler.java
@@ -0,0 +1,35 @@
+package guru.springframework.webfluxstockquoteservice.web;
+
+import guru.springframework.webfluxstockquoteservice.model.Quote;
+import guru.springframework.webfluxstockquoteservice.service.QuoteGeneratorService;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+
+/**
+ * Created by jt on 10/18/17.
+ */
+@Component
+public class QuoteHandler {
+    private final QuoteGeneratorService quoteGeneratorService;
+
+    public QuoteHandler(QuoteGeneratorService quoteGeneratorService) {
+        this.quoteGeneratorService = quoteGeneratorService;
+    }
+
+    public Mono<ServerResponse> fetchQuotes(ServerRequest request) {
+        int size = Integer.parseInt(request.queryParam("size").orElse("10"));
+
+        return ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(this.quoteGeneratorService.fetchQuoteStream(Duration.ofMillis(100))
+                        .take(size), Quote.class);
+    }
+}
